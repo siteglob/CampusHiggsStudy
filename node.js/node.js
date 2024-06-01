decbd56fbac124a8be97b20378ca68dfcbd8f8ae
siteglob <siteglob@gmail.com>
2024-06-01 01:11:50 -0500

    v.1.0.b1

--- node.js/node.js
+++ node.js/node.js
@@ -1,5 +1,5 @@
 const { MongoClient } = require('mongodb');
-const uri = "mongodb://localhost:27017";
+const uri = "mongodb+srv://siteglob:aeronavegabilidad@clusterhiggs.eps9f2b.mongodb.net/";
 
 async function run() {
   const client = new MongoClient(uri);
--- node.js/server.js
+++ node.js/server.js
@@ -1,7 +1,8 @@
-require('dotenv').config();
+/*require('dotenv').config();
 const mongoose = require('mongoose');
 const uri = process.env.MONGODB_URI;
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
+*/
